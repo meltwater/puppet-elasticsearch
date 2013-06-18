b81566f5f7a4c8549d6d6be6531f07c430d9111d
William Leese <william.leese@meltwater.com>
2013-06-18 14:46:33 +0900

    symlink fix for /etc/elasticsearch, puppet-lint cleanup

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,159 +5,170 @@
 # Usage:
 # include elasticsearch
 
-class elasticsearch( $version = "0.15.2", $xms = "256m", $xmx = "2048m", $user = "elasticsearch", $basepath = "/usr/local", $javahome = "/usr/lib/jvm/java", $clustername = "elasticsearch", $nodedata = true, $datapath = "default", $tcpcompress = false, $spmkey = "none", $threadpools = false ) {
-      $esBasename       = "elasticsearch"
-      $esName           = "${esBasename}-${version}"
-      $esPath           = "${basepath}/elasticsearch"
-      
-      if $datapath == "default" {
-        $esDataPath     = "${esPath}/data"
-      }
-      else {
-        $esDataPath     = $datapath
-      }
+class elasticsearch(
+  $version     = '0.15.2',
+  $xms         = '256m',
+  $xmx         = '2048m',
+  $user        = 'elasticsearch',
+  $basepath    = '/usr/local',
+  $javahome    = '/usr/lib/jvm/java',
+  $clustername = 'elasticsearch',
+  $nodedata    = true,
+  $datapath    = 'default',
+  $tcpcompress = false,
+  $spmkey      = 'none',
+  $threadpools = false
+) {
 
-      $esLibPath        = $esDataPath
-      $esLogPath        = "/var/log/${esBasename}"
-      $esXms            = $xms
-      $esXmx            = $xmx
-      $esTCPPortRange   = "9300-9399"
-      $esHTTPPortRange  = "9200-9299"
-      $esUlimitNofile   = "64000"
-      $esUlimitMemlock  = "unlimited"
-      $esPidpath        = "/var/run"
-      $esPidfile        = "${esPidpath}/${esBasename}.pid"
-      $esJarfile        = "${esName}.jar"
-      $esTCPCompress    = $tcpcompress
+  $esBasename       = 'elasticsearch'
+  $esName           = "${esBasename}-${version}"
+  $esPath           = "${basepath}/elasticsearch"
 
-      # SPM settings
-      $esSPMkey         = $spmkey
-      $esSPMjar         = "/spm/spm-monitor/lib/spm-monitor-es-1.6.0-withdeps.jar"
-      $esSPMconfig      = "/spm/spm-monitor/conf/spm-monitor-config-${spmkey}-default.xml"
-      
-      
-      
+  if $datapath == 'default' {
+    $esDataPath     = "${esPath}/data"
+  }
+  else {
+    $esDataPath     = $datapath
+  }
 
-     file { "/etc/security/limits.d":
-         ensure => directory,
-         owner => root,
-         group => root,
-     }
+  $esLibPath        = $esDataPath
+  $esLogPath        = "/var/log/${esBasename}"
+  $esXms            = $xms
+  $esXmx            = $xmx
+  $esTCPPortRange   = '9300-9399'
+  $esHTTPPortRange  = '9200-9299'
+  $esUlimitNofile   = '64000'
+  $esUlimitMemlock  = 'unlimited'
+  $esPidpath        = '/var/run'
+  $esPidfile        = "${esPidpath}/${esBasename}.pid"
+  $esJarfile        = "${esName}.jar"
+  $esTCPCompress    = $tcpcompress
 
-     file { "/etc/security/limits.d/${esBasename}.conf":
-          ensure => present,
-          content => template("elasticsearch/elasticsearch.limits.conf.erb"),
-          owner => root,
-          group => root,
-     }
+  # SPM settings
+  $esSPMkey         = $spmkey
+  $esSPMjar         = '/spm/spm-monitor/lib/spm-monitor-es-1.6.0-withdeps.jar'
+  $esSPMconfig      = "/spm/spm-monitor/conf/spm-monitor-config-${spmkey}-default.xml"
 
-     # Make sure we have the application path
-     file { "${basepath}/src":
-           ensure     => directory,
-           owner      => $user,
-           group      => $user, 
-           recurse    => true
-      }
-      
-      # download and extract archive
-      archive { "elasticsearch-${version}":
-        url => "http://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-${version}.tar.gz",
-        target => "${basepath}/src",
-        src_target => "${basepath}/src",
-        checksum => false,
-        allow_insecure => true,
-      }
+  file { '/etc/security/limits.d':
+    ensure => directory,
+    owner  => root,
+    group  => root,
+  }
 
-      # link the new version to the installation dir
-      file { $esPath:
+  file { "/etc/security/limits.d/${esBasename}.conf":
+    ensure  => present,
+    content => template('elasticsearch/elasticsearch.limits.conf.erb'),
+    owner   => root,
+    group   => root,
+  }
+
+  # Make sure we have the application path
+  file { "${basepath}/src":
+    ensure     => directory,
+    owner      => $user,
+    group      => $user,
+    recurse    => true
+  }
+
+  # download and extract archive
+  archive { "elasticsearch-${version}":
+    url            => "http://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-${version}.tar.gz",
+    target         => "${basepath}/src",
+    src_target     => "${basepath}/src",
+    checksum       => false,
+    allow_insecure => true,
+  }
+
+  # link the new version to the installation dir
+  file { $esPath:
+    ensure  => link,
+    target  => "${basepath}/src/elasticsearch-${version}",
+    require => Archive["elasticsearch-${version}"]
+  }
+
+  exec { "chown ${esPath}":
+    command => "chown -R ${user}: ${esPath}",
+    require => File[$esPath]
+  }
+
+  case $esDataPath {
+    "${esPath}/data": {}
+    default: {
+
+      # ensure link to real data path exists
+      file { "${esPath}/data":
         ensure  => link,
-        target => "${basepath}/src/elasticsearch-${version}",
-        require => Archive["elasticsearch-${version}"]
+        force   => true,
+        target  => $esDataPath,
+        require => File[$esDataPath],
       }
+    }
+  }
 
-      exec { "chown ${esPath}":
-        command => "chown -R ${user}: ${esPath}",
-        require => File[$esPath]
-      }
+  # Ensure the data path is created
+  file { $esDataPath:
+    ensure  => directory,
+    owner   => $user,
+    group   => $user,
+    require => File[$esPath],
+    recurse => true,
+  }
 
-      case $esDataPath {
-        "${esPath}/data": {}
-        default: {
+  # Symlink config to /etc
+  file { "/etc/${esBasename}":
+    ensure  => link,
+    target  => "${esPath}/config",
+    require => Archive["elasticsearch-${version}"],
+  }
 
-          # ensure link to real data path exists
-          file { "${esPath}/data":
-               ensure => link,
-               force => true,
-               target => $esDataPath,
-               require => File[$esDataPath],
-          }
-        }
-      }
+  # Apply config template for search
+  file { "${esPath}/config/elasticsearch.yml":
+    content => template('elasticsearch/elasticsearch.yml.erb'),
+    require => File["/etc/${esBasename}"],
+  }
 
-      # Ensure the data path is created
-      file { $esDataPath:
-           ensure => directory,
-           owner  => $user,
-           group  => $user,
-           require => File[$esPath],
-           recurse => true,
-      }
-      
-      # Symlink config to /etc
-      file { "/etc/${esBasename}":
-             ensure => link,
-             target => "${esPathLink}/config",
-             require => Archive["elasticsearch-${version}"],
-      }
+  #Apply logging template for search
+  file { "${esPath}/config/logging.yml":
+    content => template('elasticsearch/logging.yml.erb'),
+    require => File["/etc/${esBasename}"],
+  }
 
-      # Apply config template for search
-      file { "${esPath}/config/elasticsearch.yml":
-             content => template("elasticsearch/elasticsearch.yml.erb"),
-             require => File["/etc/${esBasename}"],      
-      }
-      
-	  # Apply logging template for search
-      file { "${esPath}/config/logging.yml":
-             content => template("elasticsearch/logging.yml.erb"),
-             require => File["/etc/${esBasename}"],
-      }
-	  
-      # Create startup script
-      file { "/etc/init.d/elasticsearch":
-           content => template("elasticsearch/elasticsearch.init.d.erb"),
-           owner  => root,
-           group  => root,
-           mode   => '0744',
-      }
+  # Create startup script
+  file { '/etc/init.d/elasticsearch':
+    content => template('elasticsearch/elasticsearch.init.d.erb'),
+    owner   => root,
+    group   => root,
+    mode    => '0744',
+  }
 
-      # Apply startup config shell script
-      file { "${esPath}/bin/elasticsearch.in.sh":
-              content => template("elasticsearch/elasticsearch.in.sh.erb"),
-              require => File["/etc/${esBasename}"],
-      }
-      
-      # Ensure logging directory
-      file { $esLogPath:
-           ensure    => directory,
-           owner     => $user,
-           group     => $user,
-           recurse   => true,
-           require => Archive["elasticsearch-${version}"],
-      }
+  # Apply startup config shell script
+  file { "${esPath}/bin/elasticsearch.in.sh":
+    content => template('elasticsearch/elasticsearch.in.sh.erb'),
+    require => File["/etc/${esBasename}"],
+  }
 
-      file { "${esPath}/logs":
-           ensure => link,
-           target => $esLogPath,
-           force => true,
-           require => File[$esLogPath],
-      }
-            
-      # Ensure the service is running
-      service { $esBasename:
-            ensure => running,
-            enable => true,
-            hasrestart => true,
-            require => [ File["${esPath}/logs"], File["${esPath}/config/elasticsearch.yml"] ],
-      }
+  # Ensure logging directory
+  file { $esLogPath:
+    ensure    => directory,
+    owner     => $user,
+    group     => $user,
+    recurse   => true,
+    require   => Archive["elasticsearch-${version}"],
+  }
+
+  file { "${esPath}/logs":
+    ensure  => link,
+    target  => $esLogPath,
+    force   => true,
+    require => File[$esLogPath],
+  }
+
+  # Ensure the service is running
+  service { $esBasename:
+    ensure     => running,
+    enable     => true,
+    hasrestart => true,
+    require    => [ File["${esPath}/logs"], File["${esPath}/config/elasticsearch.yml"] ],
+  }
 
 }
